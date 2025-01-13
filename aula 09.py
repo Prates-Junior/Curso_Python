498668b242aeb1bf24ef627dfd06349dc49212a5
Prates-Junior <162818439+Prates-Junior@users.noreply.github.com>
2025-01-13 09:28:13 -0300

    Add files via upload

---
+++ aula 09.py
@@ -0,0 +1,27 @@
+frase = "Curso em Video Python"
+dividido = frase.split()
+print(dividido[2][3])
+
+print(frase[3:13])
+
+
+# # Comando find procura um caractere dentro de uma string
+# print(frase.find('P'))
+#
+#
+# # upper Função que transforma a palavra em maiuscula
+# print(frase.upper())
+
+
+# Deixaa primeira letra em maiuscula
+print(frase.capitalize())
+
+# Deixa as inicias de todas as palavras em maiusculas
+print(frase.title())
+
+# Split, é usado para separar cadeias de caracteres
+print(frase.split())
+
+# uso da função Join, usada para unir caracteres novamente
+junção = "-".join(frase)
+print(junção)
